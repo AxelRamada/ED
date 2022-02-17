ddaea1bb2d3e6067d52c16ac7b3935e68a2cfe5b
axele <axele@192.168.1.33>
2022-02-17 14:09:30 +0100

    Programa base

---
+++ src/proyectoAxelRamada/AxelRamadaJugarVideojuegos.java
@@ -0,0 +1,20 @@
+package proyectoAxelRamada;
+
+public class AxelRamadaJugarVideojuegos {
+	
+	private String nombreHobby;
+	private String genero;
+	private String plataforma;
+	
+	public AxelRamadaJugarVideojuegos(String nombreHobby,String genero, String plataforma) {
+		this.genero=genero;
+		this.plataforma=plataforma;
+		this.nombreHobby=nombreHobby;
+		
+		
+		
+	}
+	
+	
+	
+}
